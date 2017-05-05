bbc02fc38a8841733c5d7715013e6e228b41d28f
LuanAlflen <luan.alflen4@gmail.com>
2017-05-05 09:28:05 -0300

    upload dos arquivos iniciais da aplicação

---
+++ css.php
@@ -0,0 +1,11 @@
+<style>
+
+    body{
+        background-color: antiquewhite;
+    }
+    h2{
+        color: deepskyblue;
+
+    }
+
+</style>
---
+++ logica.php
@@ -0,0 +1,13 @@
+<?php
+
+    $tratamentos = ['Magnifico', 'Mito', 'Bichao'];
+    $qualidades = [' o Garanhao', ' o Espetacular', ' o Pika das Galaxia'];
+    $lugares = [' de Joiville ', ' do asilo ', ' das baladas '];
+    $nome = "Luan";
+
+    $tratamento = $tratamentos[rand(0,2)];
+
+    $qualidade = $qualidades[rand(0,2)];
+
+    $lugar = $lugares[rand(0,2)];
+?>
